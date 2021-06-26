4037a31f90cb4feab6769765f83243f6ff03aadb
iqbalsyamhad <iqbaluddinsh@gmail.com>
2021-06-26 15:05:47 +0700

    init bushido template

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-import { Wrapper, Linkton, Card } from "bushido-strap";
+import { Wrapper, Card } from "bushido-strap";
 
 export default function Dashboard() {
   return (
     <Wrapper>
       <Card>
         <h1>Hello, world!</h1>
-        <Linkton to="/counter">Redux Counter</Linkton>
       </Card>
     </Wrapper>
   );
